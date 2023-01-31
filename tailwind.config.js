a05c56321b3781ed5f1467e750fcec1e3c6c431a
Damla Erva Kasal <damlaervakasal3@gmail.com>
2023-01-31 21:45:54 +0300

    created header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      // boylelikle her container classın
+      // içine mx-auto yazmaktan kurtulmuş oluuyoruz
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xl": "1140px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+
+      colors: {
+        "deym-red": "#BC1A45",
+        "deym-melon": "#FFD369",
+        "deym-grey": "#DDDDDD",
+        "deym-white": "#F7F7F7",
+      },
+    },
   },
   plugins: [],
 };
